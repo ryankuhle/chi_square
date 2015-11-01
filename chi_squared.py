1de43e5b7fff7a7dc32868651a53fcad32b28362
Ryan Kuhle <ryan@ryankuhle.com>
2015-11-01 11:35:20 -0600

    Print results of chisquare test

--- chi_squared.py
+++ chi_squared.py
@@ -1,10 +1,16 @@
 from scipy import stats
 import collections
+import pandas as pd
+import matplotlib.pyplot as plt
 
-#Load the data into a pandas, clean it
 loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
 loansData.dropna(inplace=True)
 
+freq = collections.Counter(loansData['Open.CREDIT.Lines'])
+plt.figure()
+plt.bar(freq.keys(), freq.values(), width=1)
 
-
-#Write a script called "chi_squared.py" that loads the data, cleans it, performs the chi-squared test, and prints the result. Push your code to Github and enter the link below.
+chi, p = stats.chisquare(freq.values())
+print "Chi: %s" % chi
+print "P: %s" % p
+plt.show()
