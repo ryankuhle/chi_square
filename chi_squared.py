6e15928276d9cf89fc9a1d6e4c6ab0e5f12a373e
Ryan Kuhle <ryan@ryankuhle.com>
2015-11-01 11:26:36 -0600

    Load credit line data into pandas, clean NULL values

--- chi_squared.py
+++ chi_squared.py
@@ -1,1 +1,10 @@
+from scipy import stats
+import collections
+
+#Load the data into a pandas, clean it
+loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
+loansData.dropna(inplace=True)
+
+
+
 #Write a script called "chi_squared.py" that loads the data, cleans it, performs the chi-squared test, and prints the result. Push your code to Github and enter the link below.
